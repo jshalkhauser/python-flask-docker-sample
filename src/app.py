cde157eaa5c9ace410dfdd4eea72a636cf2cf6d6
Jacob Shalkhauser <jshalkhauser@gmail.com>
2023-05-05 14:16:20 -0400

    docstring: exit() git status

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+"""Module providingFunction printing python version."""
 import socket
+
+"""Module providingFunction printing python version."""
 from flask import Flask,render_template
 
 app = Flask(__name__)
 
+"""Module providingFunction printing python version."""
 @app.route("/")
 def index():
     try:
@@ -12,6 +16,6 @@ def index():
     except:
         return render_template('error.html')
 
-
+"""Module providingFunction printing python version."""
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
